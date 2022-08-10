b2129aa72252a935e385d8435a0caf6a3eb3cd11
Kristina <dead.girl.2012@mail.ru>
2022-08-10 23:49:31 +0400

    add procfile

--- src/app.ts
+++ src/app.ts
@@ -10,8 +10,6 @@ import authRouter from './routes/auth.route';
 
 const app = express();
 
-// Middleware
-
 // 1. Body Parser
 app.use(express.json({ limit: '10kb' }));
 
@@ -59,7 +57,7 @@ app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   });
 });
 
-const port = config.get<number>('port');
+const port = config.get<number>('port') || 8000;
 app.listen(port, () => {
   console.log(`Server started on port: ${port}`);
   // 👇 call the connectDB function here
